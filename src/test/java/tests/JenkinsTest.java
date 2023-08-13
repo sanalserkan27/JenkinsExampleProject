3c2d8541d5b0c17f9c6b611a50c12c6477decda1
Serkan Sanal <sanalserkan27@icloud.com>
2023-08-13 20:28:01 +0300

    Serkan/v1

---
+++ src/test/java/tests/JenkinsTest.java
@@ -0,0 +1,52 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class JenkinsTest {
+
+    @Test
+    void Test1(){
+        assertTrue(true);
+    }
+    @Test
+    void Test2(){
+        assertTrue(true);
+    }
+    @Test
+    void Test3(){
+        assertTrue(true);
+    }
+    @Test
+    void Test4(){
+        assertTrue(false);
+    }
+    @Test
+    void Test5(){
+        assertTrue(true);
+    }
+    @Test
+    void Test6(){
+        assertTrue(true);
+    }
+    @Test
+    void Test7(){
+        assertTrue(true);
+    }
+    @Test
+    void Test8(){
+        assertTrue(true);
+    }
+    @Test
+    void Test9(){
+        assertTrue(true);
+    }
+    @Test
+    void Test10(){
+        assertTrue(true);
+    }
+
+
+
+}
